89ac64b0196ad05303c84ccf36fb9fe6aebc9b21
Zoravur Singh <zoravur@rossintelligence.com>
2019-07-28 01:33:28 -0400

    Updates

--- src/constants/Fonts.js
+++
@@ -1,6 +0,0 @@
-const Fonts = {
-  Handwritten: 'Bad Script,Shadows Into Light Two,serif',
-  HandwrittenTitle: 'Just Another Hand,sans-serif',
-}
-
-export default Fonts
--- src/constants/colors.js
+++
@@ -1,8 +0,0 @@
-const Colors = {
-  MediumGrey: '#999999',
-  DarkGrey: '#444444',
-  Black: '#000000',
-  Accent: '#00a',
-}
-
-export default Colors
